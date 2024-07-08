a07efa4e3276033e3edea4ede3fb5a8a8c88f9b6
Jonathan Marshall <jmarshall@never.you.mind>
2024-07-09 11:39:28 +1200

    adds lab2

---
+++ labs/lab02.R
@@ -0,0 +1,19 @@
+# Our second R script
+
+# Lines starting with a # are comments. You can write anything you like there and
+# it will be ignored when 'run' in R/RStudio. These are useful for adding notes
+# to yourself for later.
+
+# This first command loads up the `tidyverse` library/package which includes
+# `ggplot2` for creating pretty charts.
+library(tidyverse)
+
+# Now we'll load some data on earthquakes from around Fiji
+data(quakes)
+
+# Let's plot the earthquake magnitude versus the number
+# of stations that detect them
+ggplot(data=quakes) +
+  geom_point(mapping=aes(x=mag, y=stations))
+
+# You can add more code or comments below here.
